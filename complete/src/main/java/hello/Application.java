60167d2ce3c6c027d9b1acadcdc800e0c7d33672
Juan Ortiz Márquez <juanortizmarquez005@gmail.com>
2021-09-10 13:52:25 +0200

    prueba v2

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Esto es una prueba v2";
 	}
 
 	public static void main(String[] args) {
